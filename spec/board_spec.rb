269d787a7158af893eb78121c62cea20c2941757
Duggan Roberts <dugganroberts@gmail.com>
2019-09-16 10:33:18 -0500

    removed game logic from board spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,20 +2,12 @@ require 'spec_helper'
 
 describe Board do
   let(:board) {Board.new}
-
   describe "Create new array and check if position valid" do
     it "has a valid postion" do
       expect(board.valid_position?(1)).to eq(true)
     end
-    it "should fail if position is not valid" do
-      expect(board.valid_position?(10)).to raise_error
-    end
     it "should return available positons on get_available_positions" do
       expect(board.get_available_positions).to eq([1,2,3,4,5,6,7,8,9])
-    end
-
-    it "chosen spot to see if it's available" do
-      expect(board.chosen_spot? 3).to eq.(true)
     end
   end
 end
